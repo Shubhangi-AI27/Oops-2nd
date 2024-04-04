bf89de1602be4e1e379b782ade50476cab56d2e6
Shubhangi-AI27 <158279174+Shubhangi-AI27@users.noreply.github.com>
2024-04-04 22:31:35 +0530

    Add files via upload

---
+++ simpleinterest.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int p,r,t,si;
+    cout<<"enter value of p";
+    cin>>p;
+    cout<<"enter value of r";
+    cin>>r;
+    cout<<"enter value of t";
+    cin>>t;
+    si= (p*r*t)/1000;
+    cout<<"simple interest is:"<<si;
+    return 0;
+}
